2b809831dc48a4ec72eaa60ccc7c69686fbef119
Patryk Szaruga <105669397+Patrick2210@users.noreply.github.com>
2023-04-26 14:37:26 +0100

    Add files via upload

--- src/main/java/com/szaruga/enums/Strings.java
+++ src/main/java/com/szaruga/enums/Strings.java
@@ -4,7 +4,7 @@ public enum Strings {
     VERTICAL_BAR("|"),
     SIZE_MAP("Max size of map is 25x25"),
     FIX_SIZE("Please fix size of map"),
-    GOODBYE("Goodbye"),
+    GOODBYE("\nGoodbye"),
     WELCOME("Welcome in WarShips\n"),
     START("Start game please press: NUMBER ZERO "),
     RDY_SHIP("Ready to set up ship?"),
@@ -16,20 +16,19 @@ public enum Strings {
     PRESS_ONE("Press: Number 1 -> show map\n"),
     PRESS_TWO("Press: Number 2 -> set up your fleet\n"),
     PRESS_THREE("Press: Number 3 -> "),
-    SHOOT_THEM_ALL(" shoot them all!"),
+    SHOOT_THEM_ALL(" shot them all!"),
     PRESS_ZERO("Press Zero to show menu."),
     SQUARE_SHOT("That square was already shot"),
-    STILL_GOT("Still you got "),
-    SHIPS_LEFT(" ships to shoot!"),
+    REMAINING_SHIPS("Remainig on battlefield: "),
+    MORE_LEFT(" more to shoot!"),
     CONGRATULATION("Congratulation you"),
-    OCCUPIED("This square if already occupied by ship"),
+    OCCUPIED("This position is already occupied by ship..."),
     MAP_DECRIPTION("\nMAP DECRIPTION:\n"),
     NUMBER_ZERO("Number 0 on map means -> WATER\n"),
     NUMBER_ONE("Number 1 on map means -> SHIP\n"),
-    // nie ma takiego słowa jak HIITED, jest HIT
-    NUMBER_TWO("Number 2 on map means -> HITTED SHIP\n"),
-    // tu raczej nie MISSED SHIP a missed SHOT
-    NUMBER_THREE("Number 3 on map means -> MISSED SHIP");
+    NUMBER_TWO("Number 2 on map means -> HIT SHIP\n"),
+    NUMBER_THREE("Number 3 on map means -> MISSED SHOOT"),
+    BEFORE_SHOOTING("Before shooting you must set up ships on map...\n");
     public final String string;
 
     Strings(String str) {

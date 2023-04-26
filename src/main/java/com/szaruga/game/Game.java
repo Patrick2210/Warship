59090a9ea30dd9fa76a892e8cb0cc21bc6907358
Patryk Szaruga <105669397+Patrick2210@users.noreply.github.com>
2023-04-26 14:37:46 +0100

    Add files via upload

--- src/main/java/com/szaruga/game/Game.java
+++ src/main/java/com/szaruga/game/Game.java
@@ -6,44 +6,50 @@ import com.szaruga.map.WarshipMap;
 
 import java.util.Scanner;
 
-
-// ogólna ważna rzecz, jak używasz klasy która otwiera albo używa zasobów systemu operacyjnego to musisz ją później zamknąć metodą
-// .close() lub użyć try-with-resources, żeby zwolnić zasoby, bo inaczej aplikacja będzie pożerała zasoby
-// To się tyczy wszystkich operacji na plikach i input/output streamach, czyli na przykład klasa Scanner
 public class Game {
     private WarshipMap map;
+    int shipsOnMap;
     SupportClass sc = new SupportClass();
 
     public Game(WarshipMap map) {
-        if (map.getHeight() > 25 && map.getWidth() > 25) {
-            System.out.println(FIX_SIZE.string);
-        } else {
-            this.map = map;
+        if (map != null) {
+            if (map.getHeight() > 25 && map.getWidth() > 25) {
+                System.out.println(FIX_SIZE.string);
+            } else {
+                this.map = map;
+            }
         }
-
     }
 
-    // metoda start nie startuje gry tak naprawdę tylko printuje powitalnego stringa, ja bym to wywalił, a printa wrzucił tam gdzie jest start() używany
-    private void start() {
-        System.out.println(WELCOME.string + START.string);
+    private boolean isShip(int row, int col) {
+        int checkSquare = map.getSquare(row, col);
+        return checkSquare == 1;
     }
 
-    // ta metoda sugeruje że coś się dzieje z pozycją statku, a tymczasem ta metoda ustawia statek, więc powinna się nazywać
-    // positiionShip albo putShip
-    private void shipPosition() {
+    private boolean checkSquare(int row, int col) {
+        int checkSquare = map.getSquare(row, col);
+        switch (checkSquare) {
+            case 2, 3 -> {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void putShip() {
         Scanner row = new Scanner(System.in);
         Scanner col = new Scanner(System.in);
         System.out.println(RDY_SHIP.string);
         System.out.println(ROW.string);
-
         int r = sc.rowConverter(row.next());
         System.out.println(COL.string);
         int c = col.nextInt();
-
-        if (map.getShip(r, c)) {
+        if (isShip(r, c)) {
             System.out.println(OCCUPIED.string);
         } else {
-            map.setShip(r, c);
+            int ship = 1;
+            map.setSquare(r, c, ship);
+            shipsOnMap++;
         }
     }
 
@@ -55,71 +61,65 @@ public class Game {
         int r = sc.rowConverter(row.next());
         System.out.println(COL.string);
         int c = col.nextInt();
-
-        if (map.isHit(r, c)) {
+        if (checkSquare(r, c)) {
             System.out.println(SQUARE_SHOT.string);
         } else {
-            if (map.getSquare(r, c)) {
-                map.setHit(r, c);
+            if (isShip(r, c)) {
+                int hit = 2;
+                map.setSquare(r, c, hit);
+                shipsOnMap--;
                 System.out.println(HIT_SHIP.string);
 
-            } else if (!map.getSquare(r, c)) {
-                map.setMiss(r, c);
+            } else if (!isShip(r, c)) {
+                int miss = 3;
+                map.setSquare(r, c, miss);
                 System.out.println(MISSED_SHIP.string);
             }
         }
     }
 
     public void play() {
-        // kiedy map może być null? Już w konstruktorze można sprawdzać żeby później nie musieć pisać takich ifów, kod będzie prostszy
-        // bo będizesz wiedział żę zawsze operujesz na nie nullowej mapie bo masz zabezpieczenie "na wejściu" czyli przy tworzeniu obiekty Game
-        if (map != null) {
-            Scanner scanner = new Scanner(System.in);
-            boolean iterator = true;
-            start();
-            // tu po prostu powinieneś dać while(true), bez tej zmiennej
-            while (iterator) {
+        System.out.println(WELCOME.string + START.string);
+        /* Czy w taki sposob jak to napisalem bedzie scanner sie automatycznie zamykac,
+        * na necie pisali ze niby tak wole sie upewnic.
+        * Jezeli zrobilem to dobrze to gitowa. Czy musze zamykac kazdego scanera z osobna w kazdej metodzie?
+        */
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
                 int i = scanner.nextInt();
                 if (i == 0) {
                     System.out.println(PRESS_ONE.string + PRESS_TWO.string + PRESS_THREE.string + SHOOT_THEM_ALL.string);
-
                     int j = scanner.nextInt();
-                    if (j == 1) {
-                        map.showMap();
-                        System.out.println(PRESS_ZERO.string);
-                    }
-                    if (j == 2) {
-                        shipPosition();
-                        System.out.println(PRESS_ZERO.string);
-                    }
-                    if (j == 3) {
-                        // tu powinieneś sprawdzać czy w ogóle jest w co strzelać, innymi słowy czy mapa jest pusta
-                        shoot();
-                        /* mam watpliwosci odnosnie ponizszej lini, gdyz tak naprawde 2x robi sie ten sam proces
-                         * w 90 chcialbym tylko wyluskac wartosc inta, a nie robic cala petle od nowa, bo to zrobilem w 89
-                         */
-                        // masz rację, powinieneś raz zawołać tą funkcję i jej wynik zapisać do zmiennej w klasie Game
-                        // klasa Game kontroluję grę i to co się dzieje więc warto żeby wiedziała ile statków zostało na mapie
-                        // ogólnie możesz zrobić tak, że jak ustawiasz statki to zapisujesz do zmiennej ile ich ustawiłeś
-                        // i po każdym trafieniu w statek odejmujesz od tej zmiennej, wtedy nie bedziesz musiał wykonywać
-                        // całej tej operacji jaka jest w map.getLeftShips()
-                        if (map.getLeftShips() != 0) {
-                            System.out.println(STILL_GOT.string + map.getLeftShips() + SHIPS_LEFT.string);
+                    switch (j) {
+                        case 1 -> {
+                            map.showMap();
                             System.out.println(PRESS_ZERO.string);
-                        } else {
-                            // jak zestrzelisz wszystkie statki to można zabić program po wyświetleniu komunikatu
-                            // bo tak to nie wiadomo co zrobić jak jesteś użytkownikiem
-                            System.out.println(CONGRATULATION.string + SHOOT_THEM_ALL.string);
                         }
-                        // ten warunek możesz zamienić na j > 3, a najlepiej to użyć instrukcji switch żeby obsłużyć tą zmienną "j"
-                    } else if ((j != 1) && (j != 2) && (j != 3)) {
-                        System.out.println(PRESS_ZERO.string);
+                        case 2 -> {
+                            putShip();
+                            System.out.println(PRESS_ZERO.string);
+                        }
+                        case 3 -> {
+                            if (shipsOnMap == 0) {
+                                System.out.println(BEFORE_SHOOTING.string + PRESS_ZERO.string);
+                            } else {
+                                shoot();
+                                if (shipsOnMap > 0) {
+                                    System.out.println(REMAINING_SHIPS.string + shipsOnMap + MORE_LEFT.string);
+                                    System.out.println(PRESS_ZERO.string);
+                                } else {
+                                    System.out.println(CONGRATULATION.string + SHOOT_THEM_ALL.string + GOODBYE.string);
+                                    System.exit(0);
+                                }
+                            }
+                        }
                     }
-                    // ten else if bez sensu, wystarczy else
-                } else if (i != 0) {
+                } else {
                     System.out.println(PRESS_ZERO.string);
                 }
             }
-        } else System.out.println(GOODBYE.string);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/com/szaruga/game/SupportClass.java
+++ src/main/java/com/szaruga/game/SupportClass.java
@@ -4,86 +4,8 @@ import static com.szaruga.enums.CapitalLetters.*;
 
 public class SupportClass {
 
-    // to jest dobrze ale można zrobić prostszy kod
-    // zobacz sobie jakie enum ma metody i jak to możesz wykorzystać
+    //Czy chodzilo Ci o takie rozwiazanie? ordinal() zwraca mi inta wgl kolejnosci ulokowania w Enumie
     protected Integer rowConverter(String letter) {
-        if (letter.equals(A.letter)) {
-            return 0;
-        }
-        if (letter.equals(B.letter)) {
-            return 1;
-        }
-        if (letter.equals(C.letter)) {
-            return 2;
-        }
-        if (letter.equals(D.letter)) {
-            return 3;
-        }
-        if (letter.equals(E.letter)) {
-            return 4;
-        }
-        if (letter.equals(F.letter)) {
-            return 5;
-        }
-        if (letter.equals(G.letter)) {
-            return 6;
-        }
-        if (letter.equals(H.letter)) {
-            return 7;
-        }
-        if (letter.equals(I.letter)) {
-            return 8;
-        }
-        if (letter.equals(J.letter)) {
-            return 9;
-        }
-        if (letter.equals(K.letter)) {
-            return 10;
-        }
-        if (letter.equals(L.letter)) {
-            return 11;
-        }
-        if (letter.equals(M.letter)) {
-            return 12;
-        }
-        if (letter.equals(N.letter)) {
-            return 13;
-        }
-        if (letter.equals(O.letter)) {
-            return 14;
-        }
-        if (letter.equals(P.letter)) {
-            return 15;
-        }
-        if (letter.equals(Q.letter)) {
-            return 16;
-        }
-        if (letter.equals(R.letter)) {
-            return 17;
-        }
-        if (letter.equals(S.letter)) {
-            return 18;
-        }
-        if (letter.equals(T.letter)) {
-            return 19;
-        }
-        if (letter.equals(U.letter)) {
-            return 20;
-        }
-        if (letter.equals(V.letter)) {
-            return 21;
-        }
-        if (letter.equals(W.letter)) {
-            return 22;
-        }
-        if (letter.equals(X.letter)) {
-            return 23;
-        }
-        if (letter.equals(Y.letter)) {
-            return 24;
-        } else if (letter.equals(Z.letter)) {
-            return 25;
-        }
-        return 0;
+        return valueOf(letter).ordinal();
     }
 }
